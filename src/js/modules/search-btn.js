4329cf055ab4c7c33212d3c355542395bce3251a
slava <asperatuz@yandex.ru>
2022-07-26 12:32:07 +0900

    feat: add search page with responsive

--- src/js/modules/search-btn.js
+++ src/js/modules/search-btn.js
@@ -1,7 +1,9 @@
 const searchBtn = document.querySelector(".nav-search");
 const closeSearchBar = document.querySelector(".closebtn-search");
 const inputSearch = document.querySelector(".search-bar-input");
+const inputSearchPage = document.querySelector(".search-bar--in");
 const searchOverlay = document.querySelector(".search-overlay");
+const closeBtnInput = document.querySelector(".close-btn-search--page");
 
 let html = document.documentElement;
 
@@ -16,3 +18,22 @@ function closeSearch() {
 
 searchBtn.addEventListener("click", openSearch);
 closeSearchBar.addEventListener("click", closeSearch);
+inputSearchPage.addEventListener("keyup", function () {
+  if (this.value != "") {
+    document
+      .querySelector(".close-btn-search--page")
+      .classList.add("close-btn-search--active");
+    document.querySelector(".arrow-search--page").style.display = "none";
+  } else {
+    document
+      .querySelector(".close-btn-search--page")
+      .classList.remove("close-btn-search--active");
+    document.querySelector(".arrow-search--page").style.display =
+      "inline-block";
+  }
+});
+closeBtnInput.addEventListener("click", function () {
+  inputSearchPage.value = "";
+  closeBtnInput.classList.remove("close-btn-search--active");
+  document.querySelector(".arrow-search--page").style.display = "inline-block";
+});
